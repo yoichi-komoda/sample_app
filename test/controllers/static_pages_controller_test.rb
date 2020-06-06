6a01970bcbf22c66a36416549ba3062f5cfc586b
yoichi.komoda <nagisasendou.biz@gmail.com>
2020-06-06 15:22:50 +0000

    finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,11 +6,16 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
   
-  test "should get home" do
-    get static_pages_home_url
+  test "should get root" do
+    get root_url
     assert_response :success
-    assert_select "title", "Home|#{@base_title}"
   end
+  
+  # test "should get home" do
+  #   get static_pages_home_url
+  #   assert_response :success
+  #   assert_select "title", "Home|#{@base_title}"
+  # end
 
   test "should get help" do
     get static_pages_help_url
@@ -22,6 +27,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_about_url
     assert_response :success
     assert_select "title", "About|#{@base_title}"
+  end
+    
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact|#{@base_title}"
   end
 
 end
