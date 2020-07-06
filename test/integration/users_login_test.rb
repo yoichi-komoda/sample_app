140f50258f78248f9553787d8ddc8476fafbc2d2
yoichi.komoda <nagisasendou.biz@gmail.com>
2020-07-06 07:28:09 +0000

    Implement basic login

---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,50 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user = users(:michael)
+  end
+  
+  test "login with invalid information" do #無効な情報でログインするテスト
+    get login_path #ログインページにアクセスする
+    assert_template 'sessions/new' #指定のビューを表示させれているか
+    post login_path, params: { session: { email: "", password: "" } } #無効なparamsを使う
+    assert_template 'sessions/new' #無効な情報なので当然renderされる
+    assert_not flash.empty? #flashが存在していればtrue
+    get root_path #別ページへアクセス
+    assert flash.empty? #flashが消えていればtrue
+  end
+  
+  test "login with valid information" do
+    get login_path
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
+  
+  test "login with valid information followed by logout" do
+    get login_path
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+  end
+end
